b255addb005d61563e01375b800ad73bf86a9b2c
Pablo Ivan Maurig <maurigpi@dlatv.net>
2020-09-29 16:18:27 -0300

    update

---
+++ 252f366e-ad5baa3e8beec19bd2b8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{NIcq:function(n,t,a){"use strict";a.d(t,"a",(function(){return e}));var c=a("Lnxd"),e=function(n){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"}}]})(n)};e.displayName="MdPhoneIphone"}}]);
+//# sourceMappingURL=252f366e-ad5baa3e8beec19bd2b8.js.map
